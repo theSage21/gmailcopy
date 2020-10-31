3d3fedafca9750f65dd805c713ef4830b18aa71f
arjoonn sharma <arjoonn.94@gmail.com>
2020-10-31 15:50:45 +0530

    fix typo

--- gmailcopy/server.py
+++ gmailcopy/server.py
@@ -99,7 +99,7 @@ def run(backup_dir):
             ids = set()
             for q in qlist:
                 ids |= revidx[q]
-            listed = dict(sorted((k, meta[k]) for k in ids, key=lambda x: x[1].stamp))
+            listed = dict(sorted(((k, meta[k]) for k in ids), key=lambda x: x[1].stamp))
         return render_template(
             "listing.html", listed=listed, qlist=qlist, n_listed=len(listed)
         )
